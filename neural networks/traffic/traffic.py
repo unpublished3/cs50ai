372cdada58ea3adc74cb5284624ba5d1a9e04ec1
unpublished3 <chapagainsakshyam@gmail.com>
2024-08-04 09:02:29 +0545

    Model compiles and works

--- neural networks/traffic/traffic.py
+++ neural networks/traffic/traffic.py
@@ -24,25 +24,25 @@ def main():
     images, labels = load_data(sys.argv[1])
 
     # Split data into training and testing sets
-    # labels = tf.keras.utils.to_categorical(labels)
-    # x_train, x_test, y_train, y_test = train_test_split(
-    #     np.array(images), np.array(labels), test_size=TEST_SIZE
-    # )
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
 
-    # # Get a compiled neural network
-    # model = get_model()
+    # Get a compiled neural network
+    model = get_model()
 
-    # # Fit model on training data
-    # model.fit(x_train, y_train, epochs=EPOCHS)
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
 
-    # # Evaluate neural network performance
-    # model.evaluate(x_test,  y_test, verbose=2)
+    # Evaluate neural network performance
+    model.evaluate(x_test, y_test, verbose=2)
 
-    # # Save model to file
-    # if len(sys.argv) == 3:
-    #     filename = sys.argv[2]
-    #     model.save(filename)
-    #     print(f"Model saved to {filename}.")
+    # Save model to file
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
 
 
 def load_data(data_dir):
@@ -72,9 +72,6 @@ def load_data(data_dir):
             images.append(data)
             labels.append(i)
 
-        # images.append(array)
-
-
     return (images, labels)
 
 
@@ -84,7 +81,27 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential()
+    model.add(
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        )
+    )
+
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Flatten())
+
+    model.add(tf.keras.layers.Dense(128, activation="relu"))
+    # model.add(tf.keras.layers.Dropout(0.5))
+
+    model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax"))
+    model.summary()
+
+    model.compile(
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    return model
 
 
 if __name__ == "__main__":
