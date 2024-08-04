fd09e374e350e353b768545c529f52c989e7337f
unpublished3 <chapagainsakshyam@gmail.com>
2024-08-04 08:08:55 +0545

    Load data

--- neural networks/traffic/traffic.py
+++ neural networks/traffic/traffic.py
@@ -9,7 +9,8 @@ from sklearn.model_selection import train_test_split
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-NUM_CATEGORIES = 43
+# NUM_CATEGORIES = 43
+NUM_CATEGORIES = 3
 TEST_SIZE = 0.4
 
 
@@ -23,25 +24,25 @@ def main():
     images, labels = load_data(sys.argv[1])
 
     # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
-    x_train, x_test, y_train, y_test = train_test_split(
-        np.array(images), np.array(labels), test_size=TEST_SIZE
-    )
+    # labels = tf.keras.utils.to_categorical(labels)
+    # x_train, x_test, y_train, y_test = train_test_split(
+    #     np.array(images), np.array(labels), test_size=TEST_SIZE
+    # )
 
-    # Get a compiled neural network
-    model = get_model()
+    # # Get a compiled neural network
+    # model = get_model()
 
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
+    # # Fit model on training data
+    # model.fit(x_train, y_train, epochs=EPOCHS)
 
-    # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
+    # # Evaluate neural network performance
+    # model.evaluate(x_test,  y_test, verbose=2)
 
-    # Save model to file
-    if len(sys.argv) == 3:
-        filename = sys.argv[2]
-        model.save(filename)
-        print(f"Model saved to {filename}.")
+    # # Save model to file
+    # if len(sys.argv) == 3:
+    #     filename = sys.argv[2]
+    #     model.save(filename)
+    #     print(f"Model saved to {filename}.")
 
 
 def load_data(data_dir):
@@ -58,7 +59,23 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images = []
+    labels = []
+
+    for i in range(NUM_CATEGORIES):
+        current_dir = os.path.join(data_dir, str(i))
+
+        for file_name in os.listdir(current_dir):
+            data = cv2.imread(os.path.join(current_dir, file_name))
+            data = np.resize(data, (IMG_WIDTH, IMG_WIDTH, 3))
+
+            images.append(data)
+            labels.append(i)
+
+        # images.append(array)
+
+
+    return (images, labels)
 
 
 def get_model():
